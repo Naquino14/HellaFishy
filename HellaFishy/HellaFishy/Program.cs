78b2802dbab9442f8c335598b1b660073eef120f
Nathaniel Aquino <37340195+Naquino14@users.noreply.github.com>
2022-04-23 18:17:57 -0400

    Create project

---
+++ HellaFishy/HellaFishy/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using c = System.Console;
+
+namespace HellaFishy
+{
+    public class Interpreter
+    {
+        public static void Main(string[] args)
+        {
+             
+        }
+    }
+}
